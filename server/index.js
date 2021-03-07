4e6ff82c45126b6c4db5937c5f7b7a1f5dcdc947
broberts2 <broc.roberts@outlook.com>
2021-03-06 21:11:17 -0700

    changes

--- server/index.js
+++ server/index.js
@@ -9,18 +9,17 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post(`/webhook`, async (req, res) => {
-	console.log("received");
-	// exec("git pull origin master", (error, stdout, stderr) => {
-	// 	if (error) {
-	// 		console.log(`error: ${error.message}`);
-	// 		return;
-	// 	}
-	// 	if (stderr) {
-	// 		console.log(`stderr: ${stderr}`);
-	// 		return;
-	// 	}
-	// 	console.log(`stdout: ${stdout}`);
-	// });
+	exec("git pull origin master", (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.log(`stderr: ${stderr}`);
+			return;
+		}
+		console.log(`stdout: ${stdout}`);
+	});
 	res.json("Success!");
 }),
 	(server = require("http").createServer(app));
